6c23eb7287cc363e74e2e3498c72c31d9f56a810
SlowLearner22 <jdelamotasanchis@gmail.com>
2020-11-10 09:57:01 +0100

    Adding average.cs

---
+++ average.cs
@@ -0,0 +1,34 @@
+﻿//Average of the marks introduced by the user
+
+using System;
+
+public class Average
+{
+
+	public static void Main()
+	{
+		double notesSum = 0, numericalMark, counterMarks = 0, average;
+		string mark;
+
+		Console.Write("Insert a mark: ");
+		mark = Console.ReadLine();
+
+		while (mark != "end")
+		{
+			numericalMark = Convert.ToDouble(mark);
+
+			counterMarks++;
+
+			notesSum += numericalMark;
+
+			Console.Write("Insert a mark: ");
+			mark = Console.ReadLine();
+		}
+
+		if (counterMarks != 0)
+		{
+			average = notesSum / counterMarks;
+			Console.WriteLine("The average is {0}.", average);
+		}
+	}
+}
